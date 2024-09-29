85fe92c0eb3af8ce44a0c440011f7ee1befd99fd
liuzihaohao <liuzihaohao@users.noreply.github.com>
2024-09-29 10:48:40 +0000

    init

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'HCCPDC Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm797754523-97a2a746f68d0261fac144bb',
+  ],
+
+  // 日志天数
+  CountDays: 365,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://status.org.cn/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/yb/uptime-status'
+    },
+    {
+      text: 'Blog',
+      url: 'https://abo.xyz/'
+    },
+  ],
+};
